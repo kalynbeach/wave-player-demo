754769292c69a8685e010de8ac8fa1e616c2711c
kalynbeach <kalynbeach@gmail.com>
2023-08-24 16:53:16 -0700

    v1.0.0: add and use @kalynbeach/wave-player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { WavePlayerMode } from '@/lib/types'
-import WavePlayer from './wave-player/WavePlayer'
-import WavePlayerStack from './wave-player/WavePlayerStack'
+// import WavePlayer from './wave-player/WavePlayer'
+// import WavePlayerStack from './wave-player/WavePlayerStack'
+import { WavePlayer, WavePlayerStack, StackProvider } from '@kalynbeach/wave-player'
 import { tracks } from '@/lib/tracks'
 
 export default function Home() {
@@ -9,17 +12,19 @@ export default function Home() {
       <div className='flex flex-col gap-6 justify-center items-start'>
         <h1 className='mb-6 text-3xl font-mono'>wave-player</h1>
         <h2 className='mt-6 text-xl font-mono'>WavePlayer</h2>
-        <WavePlayer
-          id={0}
-          mode={WavePlayerMode.Playlist}
-          tracks={tracks}
-          isStacked={false}
-        />
-        <h2 className='mt-6 text-xl font-mono'>WavePlayerStack</h2>
-        <WavePlayerStack
-          id={0}
-          tracks={tracks}
-        />
+        <StackProvider>
+          <WavePlayer
+            id={0}
+            mode={WavePlayerMode.Playlist}
+            tracks={tracks}
+            isStacked={false}
+          />
+          <h2 className='mt-6 text-xl font-mono'>WavePlayerStack</h2>
+          <WavePlayerStack
+            id={0}
+            tracks={tracks}
+          />
+        </StackProvider>
       </div>
     </main>
   )
